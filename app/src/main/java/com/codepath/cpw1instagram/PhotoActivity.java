ab4bcd6f639dfe2d024bc9217949fd63f6a9c995
Qi Zhu <qi.zhu@airbnb.com>
2016-02-04 22:34:50 -0800

    Add pull to refresh.

--- app/src/main/java/com/codepath/cpw1instagram/PhotoActivity.java
+++ app/src/main/java/com/codepath/cpw1instagram/PhotoActivity.java
@@ -1,6 +1,7 @@
 package com.codepath.cpw1instagram;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 
@@ -19,17 +20,34 @@ public class PhotoActivity extends AppCompatActivity {
   private static final String CLIENT_ID = "e05c462ebd86446ea48a5af73769b602";
   private ArrayList<InstagramPhoto> photos;
   private InstagramPhotosAdapter aPhotos;
+  private SwipeRefreshLayout swipeContainer;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_photos);
+    // Lookup the swipe container view
+    swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+    // Setup refresh listener which triggers new data loading
+    swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+      @Override
+      public void onRefresh() {
+        fetchPopularPhotos();
+      }
+    });
+    // Configure the refreshing colors
+    swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+      android.R.color.holo_green_light,
+      android.R.color.holo_orange_light,
+      android.R.color.holo_red_light);
+
     photos = new ArrayList<>();
     aPhotos = new InstagramPhotosAdapter(this, photos);
     ListView lvPhotos = (ListView)findViewById(R.id.lvPhotos);
     lvPhotos.setAdapter(aPhotos);
     fetchPopularPhotos();
   }
+
   public void fetchPopularPhotos() {
     String url = "https://api.instagram.com/v1/media/popular?client_id=" + CLIENT_ID;
     AsyncHttpClient client = new AsyncHttpClient();
@@ -37,6 +55,7 @@ public class PhotoActivity extends AppCompatActivity {
       @Override
       public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
         JSONArray photosJson = null;
+        aPhotos.clear();
         try {
           photosJson = response.getJSONArray("data");
           for (int i = 0; i < photosJson.length(); i++) {
@@ -53,6 +72,7 @@ public class PhotoActivity extends AppCompatActivity {
             photo.publishTime = photoJson.getInt("created_time");
             photos.add(photo);
           }
+          swipeContainer.setRefreshing(false);
         } catch (JSONException e) {
           e.printStackTrace();
         }
